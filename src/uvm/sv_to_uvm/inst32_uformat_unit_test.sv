7d4938d7b6a2a03ecb6769d7eb57f52718476e97
Jeremy Ralph <jralph@xtreme-eda.com>
2019-02-07 19:01:08 -0500

    inst32_uformat_unit_test.sv created and working

---
+++ src/uvm/sv_to_uvm/inst32_uformat_unit_test.sv
@@ -0,0 +1,138 @@
+
+//###############################################################
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//  
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+//###############################################################
+
+
+`include "svunit_defines.svh"
+`include "riscv_vip_pkg.sv"
+import uvm_pkg::*;
+`include "riscv_vip_unit_test_pkg.sv"
+import riscv_vip_pkg::*;
+import riscv_vip_class_pkg::*;
+import svunit_pkg::*;
+import riscv_vip_unit_test_pkg::*;
+
+
+module inst32_sformat_unit_test;
+  import svunit_pkg::svunit_testcase;
+   
+  string name = "inst32_ut";
+  svunit_testcase svunit_ut;
+
+  //Interface and clock stuff
+  logic clk;
+  logic rstn; 
+  riscv_vip_inst_if my_if(.*);
+  riscv_vip_regfile_if regfile_if(.*);
+  riscv_vip_csr_if csr_if(.*);
+
+  //decoder for creating instructions
+  decoder_wrapper my_decoder_wrapper;   
+   
+  //===================================
+  // Build
+  //===================================
+  function void build();
+    svunit_ut = new(name);
+    my_decoder_wrapper = decoder_wrapper::type_id::create("", null);
+    uvm_config_db#(virtual riscv_vip_inst_if)::set(uvm_root::get(), "", "m_vi",my_if);
+    uvm_config_db#(virtual riscv_vip_regfile_if)::set(uvm_root::get(), "", "m_rf_vi",regfile_if);
+    uvm_config_db#(virtual riscv_vip_csr_if)::set(uvm_root::get(), "", "m_csr_vi",csr_if);   
+  endfunction
+
+  //===================================
+  // Setup for running the Unit Tests
+  //===================================
+  task setup();
+    svunit_ut.setup();
+  endtask
+
+  //===================================
+  // Here we deconstruct anything we 
+  // need after running the Unit Tests
+  //===================================
+  task teardown();
+    svunit_ut.teardown();
+  endtask
+
+  //===================================
+  // All tests are defined between the
+  // SVUNIT_TESTS_BEGIN/END macros
+  //
+  // Each individual test must be
+  // defined between `SVTEST(_NAME_)
+  // `SVTEST_END
+  //
+  // i.e.
+  //   `SVTEST(mytest)
+  //     <test code>
+  //   `SVTEST_END
+  //===================================
+  `SVUNIT_TESTS_BEGIN    
+
+  `SVTEST(inst32_uformat_cov)
+    int bins_hit = 0;
+    const int TOT_BINS = 7;
+    const int IMM_BITS = 20;   
+    const int MIN_NEG = -(2**IMM_BITS)/2;
+    const int MAX_POS = (2**IMM_BITS)/2-1;
+
+    test_u_imm(      10, 100.0*  bins_hit/TOT_BINS); //imm of        10, 0/7 bins hit
+    test_u_imm(       0, 100.0*++bins_hit/TOT_BINS); //imm of         0, 1/7 bins hit  
+    test_u_imm(      -1, 100.0*++bins_hit/TOT_BINS); //imm of  all ones, 2/7 bins hit  
+    test_u_imm(      -1, 100.0*  bins_hit/TOT_BINS); //imm of  all ones, 2/7 bins hit  
+    test_u_imm(       1, 100.0*++bins_hit/TOT_BINS); //imm of         1, 3/7 bins hit  
+    test_u_imm(       2, 100.0*++bins_hit/TOT_BINS); //imm of         2, 4/7 bins hit  
+    test_u_imm(       4, 100.0*++bins_hit/TOT_BINS); //imm of         4, 5/7 bins hit  
+    test_u_imm( MIN_NEG, 100.0*++bins_hit/TOT_BINS); //imm of   min neg, 6/7 bins hit  
+    test_u_imm( MAX_POS, 100.0*++bins_hit/TOT_BINS); //imm of   max pos, 7/7 bins hit  
+
+    //TODO: test cross
+  
+  `SVTEST_END
+
+
+  `SVUNIT_TESTS_END
+
+  task automatic test_u_imm(
+                      imm_high_t imm,
+                      real exp_cov
+                      );
+
+    inst32_uformat i32u; 
+    imm_high_t gotten_imm; 
+    real cov;
+      
+    i32u = inst32_uformat::new_from_op_imm(my_decoder,LUI_MAP,imm);    
+    
+    gotten_imm = i32u.get_imm();
+    `FAIL_UNLESS_LOG(gotten_imm == imm, $psprintf("gotten_imm = %d, exp imm=%d", gotten_imm, imm));   
+
+    //Sample and check coverage
+    i32u.sample_cov();          
+    cov = i32u.get_imm_cov();
+    `FAIL_UNLESS_LOG(int'(cov) == int'(exp_cov), $psprintf("U imm_cov = %d, expect %d",cov,exp_cov))
+
+  endtask
+
+
+
+endmodule
